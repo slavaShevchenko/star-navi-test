74408fa276764da16226fb372da0b29de48167be
slavaShevchenko <40766327+slavaShevchenko@users.noreply.github.com>
2023-12-21 13:52:21 +0100

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  max_line_length: 120,
+  tab_width: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSameLine: true,
+}
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
@@ -9,12 +9,12 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
